333d7dd00985dfa29f843ce59ede248372efc959
Hank Fridell <hf.fridell@gmail.com>
2016-02-03 23:06:51 -0600

    --App icon --menu --about activity --context menu

--- app/src/main/java/edu/umkc/hfridell/umpirebuddy/MainActivity.java
+++ app/src/main/java/edu/umkc/hfridell/umpirebuddy/MainActivity.java
@@ -3,23 +3,74 @@ package edu.umkc.hfridell.umpirebuddy;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.app.ActionBar;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.ActionMode;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.GridLayout;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements OnClickListener {
-
+    public final static String EXTRA_DATA = "Thank you for using Umpire Buddy";
     static final private String TAG = "Umpire Buddy";
 
     AtBat currentBatter;
+
+    // Contextual action mode menu
+    private ActionMode mActionMode;
+    private ActionMode.Callback mActionModeCallback = new ActionMode.Callback(){
+        //Called when started
+
+        @Override
+        public boolean onCreateActionMode(ActionMode mode, Menu menu) {
+            MenuInflater inflater = mode.getMenuInflater();
+            inflater.inflate(R.menu.context_menu, menu);
+            return true;
+        }
+
+        //Called when actionMenu is shown
+
+        @Override
+        public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
+            return false; // false if nothing done
+        }
+
+        @Override
+        public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.contextStrike:
+                    currentBatter.strike();
+                    updateDisplay();
+
+                    mode.finish(); // closes contextual actionBar
+                    return true;
+                case R.id.contextBall:
+                    currentBatter.ball();
+                    updateDisplay();
+
+                    mode.finish();
+                    return true;
+                default:
+                    return false;
+            }
+        }
+
+        // Called when user exits action mode
+        @Override
+        public void onDestroyActionMode(ActionMode mode) {
+            mActionMode = null;
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +78,25 @@ public class MainActivity extends AppCompatActivity implements OnClickListener {
         // The following will print to LogCat.
         Log.i(TAG, "Starting onCreate...");
         setContentView(R.layout.activity_main);
+
+        // Setup contextual action mode menu (CAB)
+        // Long clicking background of main activity will
+        // bring up a contextual menu.
+        GridLayout layout = (GridLayout)findViewById(R.id.grid_layout);
+        layout.setOnLongClickListener(new View.OnLongClickListener(){
+            public boolean onLongClick(View view) {
+                // mActionMode is set back to null
+                //    above when the context menu disappears.
+                if (mActionMode != null) {
+                    return false;
+                }
+
+                // Start the CAB using the ActionMode.Callback defined above
+                mActionMode = startActionMode(mActionModeCallback);
+                view.setSelected(true);
+                return true;
+            }
+        });
 
         View strikeButton = findViewById(R.id.strikeButton);
         strikeButton.setOnClickListener(this);
@@ -100,5 +170,33 @@ public class MainActivity extends AppCompatActivity implements OnClickListener {
             }
         });
         alert.show();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu;  add items to the action bar
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here.
+
+        switch (item.getItemId()) {
+            case R.id.reset:
+                currentBatter = new AtBat();
+                updateDisplay();
+                return true;
+
+            case R.id.about:
+                Intent intent = new Intent(this, AboutActivity.class);
+                intent.putExtra(EXTRA_DATA, "Thank You for using Umpire Buddy");
+                startActivity(intent);
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
